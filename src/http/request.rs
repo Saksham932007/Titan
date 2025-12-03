5cfaf931d03016a6733e8cc631162b86a54f4f9c
Saksham932007 <sakshamkapoor810@gmail.com>
2025-12-03 15:34:50 +0530

    feat(http): implement TryFrom<&[u8]> for Request with UTF-8 handling

--- src/http/request.rs
+++ src/http/request.rs
@@ -1,5 +1,7 @@
-use super::Method;
+use super::{Method, ParseError};
 use std::collections::HashMap;
+use std::convert::TryFrom;
+use std::str;
 
 #[derive(Debug)]
 pub struct Request {
@@ -22,5 +24,19 @@ impl Request {
             method,
             headers,
         }
+    }
+}
+
+impl TryFrom<&[u8]> for Request {
+    type Error = ParseError;
+
+    fn try_from(buffer: &[u8]) -> Result<Self, Self::Error> {
+        // Convert bytes to UTF-8 string
+        let request_str = str::from_utf8(buffer)
+            .map_err(|_| ParseError::InvalidEncoding)?;
+        
+        // Placeholder for parsing logic (will implement in next commit)
+        // For now, return an error
+        Err(ParseError::InvalidRequest)
     }
 }
