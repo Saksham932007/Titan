d97d7c47d4fa626b54e59bc072cdf3f06d4e93d8
Saksham932007 <sakshamkapoor810@gmail.com>
2025-12-03 15:45:01 +0530

    feat(threadpool): add Job type alias and mpsc channel setup

--- src/thread_pool.rs
+++ src/thread_pool.rs
@@ -1,4 +1,28 @@
+use std::sync::mpsc;
 use std::thread;
+
+type Job = Box<dyn FnOnce() + Send + 'static>;
+
+pub struct ThreadPool {
+    workers: Vec<Worker>,
+    sender: mpsc::Sender<Job>,
+}
+
+impl ThreadPool {
+    pub fn new(size: usize) -> ThreadPool {
+        assert!(size > 0);
+
+        let (sender, receiver) = mpsc::channel();
+
+        let mut workers = Vec::with_capacity(size);
+
+        for id in 0..size {
+            workers.push(Worker::new(id));
+        }
+
+        ThreadPool { workers, sender }
+    }
+}
 
 pub struct Worker {
     id: usize,
