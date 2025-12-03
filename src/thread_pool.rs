1ce95ffa5b052ce23d464a5447791fc17d436e9e
Saksham932007 <sakshamkapoor810@gmail.com>
2025-12-03 15:45:39 +0530

    feat(threadpool): implement Arc<Mutex> pattern for worker job execution

--- src/thread_pool.rs
+++ src/thread_pool.rs
@@ -1,4 +1,4 @@
-use std::sync::mpsc;
+use std::sync::{mpsc, Arc, Mutex};
 use std::thread;
 
 type Job = Box<dyn FnOnce() + Send + 'static>;
@@ -13,14 +13,25 @@ impl ThreadPool {
         assert!(size > 0);
 
         let (sender, receiver) = mpsc::channel();
+        
+        // Wrap receiver in Arc<Mutex<...>> for shared ownership across workers
+        let receiver = Arc::new(Mutex::new(receiver));
 
         let mut workers = Vec::with_capacity(size);
 
         for id in 0..size {
-            workers.push(Worker::new(id));
+            workers.push(Worker::new(id, Arc::clone(&receiver)));
         }
 
         ThreadPool { workers, sender }
+    }
+
+    pub fn execute<F>(&self, f: F)
+    where
+        F: FnOnce() + Send + 'static,
+    {
+        let job = Box::new(f);
+        self.sender.send(job).unwrap();
     }
 }
 
@@ -30,9 +41,14 @@ pub struct Worker {
 }
 
 impl Worker {
-    pub fn new(id: usize) -> Worker {
-        let thread = thread::spawn(|| {
-            // Placeholder: will add job execution logic in next commit
+    pub fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Worker {
+        let thread = thread::spawn(move || loop {
+            // Lock the mutex, receive a job, and execute it
+            let job = receiver.lock().unwrap().recv().unwrap();
+            
+            println!("Worker {} executing job", id);
+            
+            job();
         });
 
         Worker { id, thread }
